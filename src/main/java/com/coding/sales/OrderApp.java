4d6ae91b589163c2c9248efcbd4d843055d63968
hehuikang <hehuikang@zybank.com.cn>
2019-07-02 18:08:35 +0800

    Merge branch 'master' of /Users/hhk/Desktop/GitProjects/kata-precious_metal_sales_system with conflicts.

--- src/main/java/com/coding/sales/OrderApp.java
+++ src/main/java/com/coding/sales/OrderApp.java
@@ -1,13 +1,34 @@
 package com.coding.sales;
 
+import com.coding.sales.dataModel.MetalProduct;
 import com.coding.sales.input.OrderCommand;
+import com.coding.sales.input.OrderItemCommand;
+import com.coding.sales.input.PaymentCommand;
 import com.coding.sales.output.OrderRepresentation;
+
+import java.math.BigDecimal;
+import java.util.*;
 
 /**
  * 销售系统的主入口
  * 用于打印销售凭证
  */
 public class OrderApp {
+    private static Map<String, Double> initOffereMap = new HashMap<String, Double>();
+    private static Map<String,String> initFullReductionMap = new HashMap<String, String>();
+    private static List<MetalProduct> initMetalProdutslist = new ArrayList<MetalProduct>();
+    static {
+
+        MetalProduct metalProduct = new MetalProduct("001001","世园会五十国钱币册",
+                "册","998.00","","");
+        initMetalProdutslist.add(metalProduct);
+
+        initOffereMap.put("9折券",0.90);
+        initOffereMap.put("9折券",0.95);
+
+        initFullReductionMap.put("每满3000元减350","");
+    }
+
 
     public static void main(String[] args) {
         if (args.length != 2) {
@@ -34,7 +55,28 @@ public class OrderApp {
         OrderRepresentation result = null;
 
         //TODO: 请完成需求指定的功能
-
+//        List<PaymentCommand> paymentCommandList = command.getPayments();
+//        PaymentCommand paymentCommand = paymentCommandList.get(0);
+//        String orderId = command.getOrderId();
+//
+//        BigDecimal amount = paymentCommand.getAmount();
+//        String memberId = command.getMemberId();
+        List<OrderItemCommand> orderItemCommandList = command.getItems();
+        double totalPrice = 0.00;
+        for (OrderItemCommand orderItemCommand : orderItemCommandList){
+            String metalProductId = orderItemCommand.getProduct();
+            for (MetalProduct metalProduct : initMetalProdutslist){
+                if (metalProduct.getMetalProductId().equals(metalProductId)){
+                    String offere = metalProduct.getMetalProductOffere();
+                    if (null != offere && !"".equals(offere)){
+                       Double doubleOffere = initOffereMap.get(offere);
+                       totalPrice = totalPrice + doubleOffere * metalProduct.getMetalProductPrice();
+                    }else{
+                        totalPrice = totalPrice + metalProduct.getMetalProductPrice();
+                    }
+                }
+            }
+        }
         return result;
     }
 }
